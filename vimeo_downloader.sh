6bbabbb88c81451032f995201ea3cdfebe910bac
JohnTESlade <john@jtes.net>
2012-06-09 21:05:12 +0100

    Updated to work with new vimeo site

--- vimeo_downloader.sh
+++ vimeo_downloader.sh
@@ -4,6 +4,7 @@
 #
 # Copyright (C) 2008, 2010  Denver Gingerich
 # Copyright (C) 2009  Jori Hamalainen
+# Copyright (C) 2012  John Slade (http://jtes.net)
 #
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
@@ -22,6 +23,7 @@ if [ $# -ne 1 ]; then
 	echo "Vimeo Downloader v0.3.1"
 	echo "by Denver Gingerich (http://ossguy.com/)"
 	echo "    with script improvements by Jori Hamalainen"
+	echo "    updated for new Vimeo site by John Slade"
 	echo
 	echo "Usage: $0 <vimeo_id|vimeo_URL>"
 	exit 1
@@ -52,11 +54,11 @@ else
 	USING_PERL=0
 fi
 
-VIDEO_XML=`${GET_CMD} http://www.vimeo.com/moogaloop/load/clip:${VIMEO_ID}`
+VIDEO_XML=`${GET_CMD} http://vimeo.com/${VIMEO_ID}`
 
 if [ $USING_PERL -eq 1 ]; then
-	REQUEST_SIGNATURE=`echo $VIDEO_XML | perl -p -e 's:^.*?\<request_signature\>(.*?)\</request_signature\>.*$:$1:g'`
-	REQUEST_SIGNATURE_EXPIRES=`echo $VIDEO_XML | perl -p -e 's:^.*?\<request_signature_expires\>(.*?)\</request_signature_expires\>.*$:$1:g'`
+	REQUEST_SIGNATURE=`echo $VIDEO_XML | perl -e '@text_in = <STDIN>; if (join(" ", @text_in) =~ /"signature":"(.*?)"/i ){ print "$1\n"; }'`
+	REQUEST_SIGNATURE_EXPIRES=`echo $VIDEO_XML | perl -e '@text_in = <STDIN>; if (join(" ", @text_in) =~ /"timestamp":(\d*?),/i ){ print "$1\n"; }'`
 	CAPTION=`echo $VIDEO_XML | perl -p -e 's:^.*?\<caption\>(.*?)\</caption\>.*$:$1:g'`
 	ISHD=`echo $VIDEO_XML |  perl -p -e 's:^.*?\<isHD\>(.*?)\</isHD\>.*$:$1:g'`
 
@@ -71,14 +73,26 @@ if [ $USING_PERL -eq 1 ]; then
 
 	FILENAME="${VIMEO_ID}-${ISHD}.flv"
 else
+
+	# TODO update the sed code to work with the new site format
+	echo "This version requires perl - exiting"
+	exit 2
+	
 	REQUEST_SIGNATURE=`echo $VIDEO_XML | sed -e 's/^.*<request_signature>\([^<]*\)<.*$/\1/g'`
 	REQUEST_SIGNATURE_EXPIRES=`echo $VIDEO_XML | sed -e 's/^.*<request_signature_expires>\([^<]*\)<.*$/\1/g'`
 	ISHD="sd"
 	FILENAME=${VIMEO_ID}.flv
 fi
 
-echo "\nDownloading video ${VIMEO_ID} to ${FILENAME}...\nRequest_signature=${REQUEST_SIGNATURE}\nRequest_signature_expires=${REQUEST_SIGNATURE_EXPIRES}\n"
-${GET_CMD} "http://www.vimeo.com/moogaloop/play/clip:${VIMEO_ID}/${REQUEST_SIGNATURE}/${REQUEST_SIGNATURE_EXPIRES}/?q=${ISHD}" > "${FILENAME}"
+echo
+echo "Downloading video ${VIMEO_ID} to ${FILENAME}..."
+echo "Request_signature=${REQUEST_SIGNATURE}"
+echo "Request_signature_expires=${REQUEST_SIGNATURE_EXPIRES}"
+echo 
+	
+${GET_CMD} "http://player.vimeo.com/play_redirect?clip_id=${VIMEO_ID}&sig=${REQUEST_SIGNATURE}&time=${REQUEST_SIGNATURE_EXPIRES}&quality=hd&codecs=H264,VP8,VP6&type=moogaloop_local&embed_location=" > "${FILENAME}"
+
+echo
 echo "Video ${VIMEO_ID} saved to ${FILENAME}"
 echo `file "${FILENAME}"`
 echo
